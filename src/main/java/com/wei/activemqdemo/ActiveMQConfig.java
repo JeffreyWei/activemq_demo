b10f4630fdb0dca35eb15f2c7c8cdd5045b5b826
wei <mavenwei@qq.com>
2015-04-29 16:28:40 +0800

    init commit

---
+++ src/main/java/com/wei/activemqdemo/ActiveMQConfig.java
@@ -0,0 +1,9 @@
+package com.wei.activemqdemo;
+
+/**
+ * Created by wei on 15/4/29.
+ */
+public final class ActiveMQConfig {
+    public static final String ACTIVEMQ="192.168.220.45:61616";
+    public static final int SEND_NUMBER = 5;
+}
