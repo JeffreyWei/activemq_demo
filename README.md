295e6466588175820d768ae8f6875c36cb3c4061
wei <mavenwei@qq.com>
2015-05-15 11:18:20 +0800

    Optimize imports

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 #ActiveMQ DEMO
 admini page		:	http://X.X.X.X:8161/admin/
 activemq port	:	61616
+activemq start  :   apache-activemq-5.11.1/bin/macosx/activemq start
 
 
--- src/main/java/com/wei/activemqdemo/ActiveMQConfig.java
+++ src/main/java/com/wei/activemqdemo/ActiveMQConfig.java
@@ -4,6 +4,6 @@ package com.wei.activemqdemo;
  * Created by wei on 15/4/29.
  */
 public final class ActiveMQConfig {
-    public static final String ACTIVEMQ="192.168.220.45:61616";
+    public static final String ACTIVEMQ="127.0.0.1:61616";
     public static final int SEND_NUMBER = 5;
 }
